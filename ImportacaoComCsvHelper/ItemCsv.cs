3cf782dee99e48fea1afa33755c659d4f4957ab4
diogoschimm <diogo_ti@hotmail.com.br>
2019-12-20 10:15:56 -0400

    Leitor de CSV

---
+++ ImportacaoComCsvHelper/ItemCsv.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportacaoComCsvHelper
+{
+    public class ItemCsv
+    {
+        public string status { get; set; }
+        public string id { get; set; } 
+        public string pedidoExt { get; set; }
+        public string referenciaExt { get; set; }
+        public string solicitacao { get; set; }
+        public string titular { get; set; }
+        public string dataAprovacao { get; set; }
+        public string situacao { get; set; }
+        public string dataSituacao { get; set; }
+        public string recusas { get; set; }
+        public string email { get; set; }
+        public string unidadeExt { get; set; }
+        public string unidade { get; set; }
+        public string extProduto { get; set; }
+        public string extProdutoNome { get; set; }
+        public string idusuario { get; set; }
+        public string usuario { get; set; }
+        public string idcertificado { get; set; }
+        public string renovacao { get; set; }
+        public string atendimentoexterno { get; set; }
+        public string certificado { get; set; }
+        public string campo1 { get; set; }
+        public string campo2 { get; set; }
+        public string campo3 { get; set; }
+        public string validade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{solicitacao};{titular};";
+        }
+    }
+}
---
+++ ImportacaoComCsvHelper/Program.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportacaoComCsvHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("C:\\Users\\Diogo\\Desktop\\Exemplo.csv"))
+            using (var csv = new CsvReader(reader))
+            {
+                var records = csv.GetRecords<ItemCsv>();
+                foreach (var item in records)
+                {
+                    Console.Write($"{item.status};");
+                    Console.Write($"{item.pedidoExt};");
+                    Console.Write($"{item.referenciaExt};");
+                    Console.Write($"{item.solicitacao};");
+                    Console.Write($"{item.titular};");
+                    Console.Write($"{item.dataAprovacao};");
+                    Console.Write($"{item.situacao};");
+                    Console.Write($"{item.dataSituacao};");
+                    Console.Write($"{item.recusas};");
+                    Console.Write($"{item.email};");
+                    Console.Write($"{item.unidadeExt};");
+                    Console.Write($"{item.unidade};");
+                    Console.Write($"{item.extProduto};");
+                    Console.Write($"{item.extProdutoNome};");
+                    Console.Write($"{item.idusuario};");
+                    Console.Write($"{item.usuario};");
+                    Console.Write($"{item.idcertificado};");
+                    Console.Write($"{item.renovacao};");
+                    Console.Write($"{item.atendimentoexterno};");
+                    Console.Write($"{item.certificado};");
+                    Console.Write($"{item.campo1};");
+                    Console.Write($"{item.campo2};");
+                    Console.Write($"{item.campo3};");
+                    Console.Write($"{item.validade};");
+
+                    Console.WriteLine("");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
